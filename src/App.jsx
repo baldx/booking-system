8590496e5764bc1e0ec0eaf2b55f6b64253d0652
Ahmad Mohamad <mr.ahmad.m2006@gmail.com>
2026-06-06 16:51:58 +0200

    add header

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
 import './App.css'
+import Header from './components/Header'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
-      
+      <Header />
+      <h1>Welcome to the Booking System</h1>
     </>
   )
 }
